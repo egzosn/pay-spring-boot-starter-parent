7811ea990e6dd8c1b662b4a72f466a72f0516623
egzosn <egzosn@gmail.com>
2019-05-09 20:50:18 +0800

    商户支付服务管理器实现，已编写 网页支付，回调，获取预订单信息

--- pay-spring-boot-autoconfigue/src/main/java/com/egzosn/pay/spring/boot/autoconfigue/PayAutoConfiguration.java
+++ pay-spring-boot-autoconfigue/src/main/java/com/egzosn/pay/spring/boot/autoconfigue/PayAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.egzosn.pay.spring.boot.autoconfigue;
 
+import com.egzosn.pay.spring.boot.core.MerchantPayServiceManager;
 import com.egzosn.pay.spring.boot.core.configurers.MerchantDetailsServiceConfigurer;
 import com.egzosn.pay.spring.boot.core.merchant.PaymentPlatform;
 import com.egzosn.pay.spring.boot.core.provider.merchant.platform.*;
@@ -38,6 +39,13 @@ public class PayAutoConfiguration {
     @ConditionalOnMissingBean(MerchantDetailsServiceConfigurer.class)
     public MerchantDetailsServiceConfigurer merchantDetails(){
         return new MerchantDetailsServiceConfigurer();
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean(MerchantPayServiceManager.class)
+    public MerchantPayServiceManager payServiceManager(){
+        return new MerchantPayServiceManager();
     }
 
 }
